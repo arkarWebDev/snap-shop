4cb8131ef4ca38a26d98f76fec2ffa0b4dd4f9c9
arkawWebDev <arkar84848484@gmail.com>
2024-09-12 00:44:04 +0630

    update-db-schema

--- server/schema.ts
+++ server/schema.ts
@@ -4,9 +4,13 @@ import {
   text,
   primaryKey,
   integer,
+  boolean,
+  pgEnum,
 } from "drizzle-orm/pg-core";
 
 import type { AdapterAccount } from "next-auth/adapters";
+
+export const RoleEnum = pgEnum("roles", ["user", "admin"]);
 
 export const users = pgTable("user", {
   id: text("id")
@@ -16,6 +20,8 @@ export const users = pgTable("user", {
   email: text("email").unique(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image"),
+  isTwoFactorEnabled: boolean("isTwoFactorEnabled").default(false),
+  role: RoleEnum("roles").default("user"),
 });
 
 export const accounts = pgTable(
