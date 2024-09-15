e205b53cfb4becc7cac3098af3149a0d07816be7
arkawWebDev <arkar84848484@gmail.com>
2024-09-16 00:28:07 +0630

    generate-token

--- server/schema.ts
+++ server/schema.ts
@@ -10,14 +10,17 @@ import {
 
 import type { AdapterAccount } from "next-auth/adapters";
 
+import { createId } from "@paralleldrive/cuid2";
+
 export const RoleEnum = pgEnum("roles", ["user", "admin"]);
 
 export const users = pgTable("user", {
   id: text("id")
     .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
+    .$defaultFn(() => createId()),
   name: text("name"),
   email: text("email").unique(),
+  password: text("password"),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image"),
   isTwoFactorEnabled: boolean("isTwoFactorEnabled").default(false),
@@ -45,5 +48,20 @@ export const accounts = pgTable(
     compoundKey: primaryKey({
       columns: [account.provider, account.providerAccountId],
     }),
+  })
+);
+
+export const emailVerificationToken = pgTable(
+  "email_verification_token",
+  {
+    id: text("id")
+      .notNull()
+      .$defaultFn(() => createId()),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+    email: text("email").notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.id, vt.token] }),
   })
 );
